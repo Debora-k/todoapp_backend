9365dcca7a382ec083026e44919b1df87bd2468d
Debora-k <debora0104k@gmail.com>
2024-10-16 15:48:33 -0600

    first commit

---
+++ controllers/task.controller.js
@@ -0,0 +1,52 @@
+const Task = require("../models/Task");
+const taskController = {}
+
+taskController.createTask = async(req,res) => {
+    try{
+        const { task, isComplete } = req.body;
+        const newTask = new Task({task,isComplete});
+        await newTask.save();
+        res.status(200).json({status:"success", data:newTask});
+    } catch(err) {
+        res.status(400).json({status:"failed", error:err});
+    }
+};
+
+taskController.getTask = async(req,res) => {
+    try {
+        const taskList = await Task.find({}).select("-__v");
+        res.status(200).json({status:"success", data:taskList});
+    } catch(err) {
+        res.status(400).json({status:"failed", error:err});
+    }
+};
+
+taskController.updateTask = async(req,res) => {
+    try {
+        const updatedTask = await Task.findById(req.params.id);
+        if(!updatedTask) {
+            throw new Error("Cannot find the task");
+        }
+        const fields = Object.keys(req.body);
+        fields.map((item) => (updatedTask[item] = req.body[item]));
+        await updatedTask.save();
+        res.status(200).json({status:"success", data:updatedTask});
+    } catch(err) {
+        res.status(400).json({status:"failed", error:err});
+
+    }
+};
+
+taskController.deleteTask = async(req, res) => {
+    try {
+        const deleteTask = await Task.findByIdAndDelete(req.params.id);
+        res.status(200).json({status:"success", data:deleteTask});
+    } catch(err) {
+        res.status(400).json({status:"failed", error:err});
+
+    }
+}
+
+
+
+module.exports = taskController;
---
+++ routes/task.api.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const taskController = require("../controllers/task.controller");
+const router = express.Router();
+
+router.post('/', taskController.createTask);
+
+router.get('/', taskController.getTask);
+
+router.put('/:id', taskController.updateTask);
+
+router.delete('/:id', taskController.deleteTask);
+
+
+module.exports= router;
+
+
